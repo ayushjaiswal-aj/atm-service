1b8342b07b6fc6f7b2f7b3b1f51af6316a3e135d
Ayush Jaiswal <ayushjaiswalaj@outlook.com>
2023-12-25 15:47:46 +0530

    Adds method to despense denominations for given currancies

---
+++ atm-service/atm-service/atmDispenser/AtmDispenser.cs
@@ -0,0 +1,39 @@
+﻿namespace atm_service.atm_dispenser
+{
+	public class AtmDispenser
+	{
+		public AtmDispenser() { }
+
+		public static List<string> getAmountDenomination(int amountToDispense)
+		{
+			List<string> amountDenominations = new List<string>();
+			int remainingAmount = amountToDispense;
+			int currancy500 = remainingAmount / 500;
+			remainingAmount = remainingAmount % 500;
+
+			int currancy200 = remainingAmount / 200;
+			remainingAmount = remainingAmount % 200;
+
+			int currancy100 = remainingAmount / 100;
+			remainingAmount = remainingAmount % 100;
+
+			int currancy50 = remainingAmount / 50;
+			remainingAmount = remainingAmount % 50;
+
+			if(remainingAmount == 0)
+			{
+				amountDenominations.Add($"Dispensing {currancy500} INR500 Note");
+				amountDenominations.Add($"Dispensing {currancy200} INR200 Note");
+				amountDenominations.Add($"Dispensing {currancy100} INR100 Note");
+				amountDenominations.Add($"Dispensing {currancy50} INR50 Note");
+            }
+			else
+			{
+				amountDenominations.Add($"Error: Cannot Dispense ammount INR{amountToDispense}.");
+			}
+
+			return amountDenominations;
+		}
+	}
+}
+
---
+++ atm-service/tests/atmDispenserTests/AtmDispenserTests.cs
@@ -0,0 +1,36 @@
+﻿using atm_service.atm_dispenser;
+
+namespace tests.atmDispenserTests
+{
+    public class AtmDispenserTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void getAmountDenomination_WhenAmountToDispenseIs850_ShouldReturnOneDinominationsOfEach()
+        {
+            // Arrange & Act
+            var result = AtmDispenser.getAmountDenomination(850);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(new List<string> { "Dispensing 1 INR500 Note",
+            "Dispensing 1 INR200 Note",
+            "Dispensing 1 INR100 Note",
+            "Dispensing 1 INR50 Note" }));
+        }
+
+        [Test]
+        public void getAmountDenomination_WhenAmountToDispenseIs860_ShouldReturnErrorMessage()
+        {
+            // Arrange & Act
+            var result = AtmDispenser.getAmountDenomination(860);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(new List<string> { "Error: Cannot Dispense ammount INR860." }));
+        }
+    }
+}
+
